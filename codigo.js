49cff08cc561f04a207cd6153aedc05703cec844
Ruan <ruan@emailfake.com>
2022-08-05 18:12:29 -0300

    Codigo1

---
+++ codigo.js
@@ -0,0 +1,15 @@
+const Lista = 15
+for (var i = 0; i <= Lista; i++) {
+    if(i !== 0){
+        console.log(i)
+    }
+    if (i == 0){
+    console.log(`Numero zero:`, i )}
+    if (i % 2 == 0){
+        console.log("\nNumero impar:")
+    }
+    else{
+        console.log("\nNumero par:")
+    }
+
+}
